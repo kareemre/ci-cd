8e68d6a3736a7a6d1822f69ceb6720fadb8f9542
kareemre <kareemrefaat@icloud.com>
2024-11-24 22:59:29 +0200

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello from Dockerized CI/CD!"));
+app.get("/", (req, res) => res.send("Helloo from Dockerized CI/CD!"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
